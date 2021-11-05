1007b06c8f4fb982fbfcc7752387ded93770c0cc
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2021-11-05 13:32:21 +0500

    minor changes

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import WeatherApp from './WeatherWidget';
+import React from "react";
+import WeatherWidget from "./WeatherWidget";
 
 const Intro = () => {
-    return (
-        <div className = 'intro'>
-            <div className = 'container intro__container'>
-                 This is Intro component 
-                <input type="text" placeholder='Where To?' /> 
-            </div>
-           
-            <div className='weather_app'><WeatherWidget /></div>
-        </div>
-    );
+  return (
+    <div className="intro">
+      <div className="container intro__container">
+        This is Intro component
+        <input type="text" placeholder="Where To?" />
+      </div>
+
+      <div className="weather_widget">
+        <WeatherWidget />
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
