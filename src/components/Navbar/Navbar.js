ef2182f8515a2e5aea781f9e1be6f4f5a37bc096
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2021-11-06 22:51:19 +0500

    added navbar classname

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,11 +3,14 @@ import  {Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='site-navbar'>
+            <div className='container'>
             <Link to='/hotels'>Hotels</Link>
             <Link to='/things_to_do'>Things to Do</Link>
             <Link to='/restaurants'>Restaurants</Link>
             <Link to='/flights'>Flights</Link>
+            </div>
+
         </div>
     );
 };
