f018ab1ccbd70e33cebf8d9b383153d0b83f8536
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2021-11-08 02:35:00 +0500

    PopularDestinationsCard styled

--- src/components/PopularDestinations/PopularDestinations.js
+++ src/components/PopularDestinations/PopularDestinations.js
@@ -1,12 +1,16 @@
 import React from "react";
-import PopularDestinationCard from './PopularDestinationsCard'
+import PopularDestinationCard from './PopularDestinationsCard';
+import styled, { css } from 'styled-components';
+import './PopularDestinations.css'
+
 
 const PopularDestinations = () => {
   return (
     <div className="popular__destinations">
       <div className="container">
         <div className="popular__destinations__inner__box">
-          <h2 className="popular__destinations__title">Popular Destinations</h2>
+          <h2 className="popular__destinations__title component-heading">Popular Destinations</h2>
+          <p className="popular__destinations__text component-text">Recommended based on your activity</p>
           <PopularDestinationCard />
         </div>
       </div>
