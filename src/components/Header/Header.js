8f1523c206912c820c2aeae265d6a54ff7ad0c5d
AbdugafurovaUmida <86460130+AbdugafurovaUmida@users.noreply.github.com>
2021-11-08 12:57:51 +0500

    headerda stilla berdim

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,24 +7,29 @@ const Header = () => {
   return (
     <header className="site__header">
       <div className="container  site__header__container">
-        <Link className="site-header-logo" to='/' >
-          <img className="site-header-logo-img" src='img/logo.svg' alt='site-header-logo' />
-        </Link>
-        <div className="">
-          <button className="" type='button'>
-            UZS
-          </button>
-          <button type='button'>
-            <img src='img/russian-flag.png' />
+  
+          <Link className="site-header-logo" to='/' >
+            <img className="site-header-logo-img" src='img/agoda_logo.png' alt='site-header-logo' />
+          </Link>
+      
+        <div className="site__header__currency_and_language">
+          <button className="currency" type='button'>UZS</button>
+
+          <button className="language" type='button'>
+            <img className="language__image" src='img/russian-flag.png' />
             {/* <img src='img/anglish-flag.png'/> */}
           </button>
+        </div>
+
+        <div className="site-header__subgroup">
           <div className="blog_pages">
-            <Link to='/review'>Review</Link>
-            <Link to='/my-trips'>Trips</Link>
+            <Link className="blog_pages__review site-header-link" to='/review'>Review</Link>
+            <Link className="blog_pages__my_trips site-header-link"to='/my-trips'>Trips</Link>
           </div>
-          <div className="authentification">
-            <Link to="/register">Register</Link>
-            <Link to="/sign-in">Sign In</Link>
+
+          <div className="auth">
+            <Link className="auth__registration site-header-link" to="/register">Register</Link>
+            <Link className="auth__sign-in site-header-link"  to="/sign-in">Sign In</Link>
           </div>
         </div>
 
