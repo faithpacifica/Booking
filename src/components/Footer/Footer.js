c08a8fd9c6e5c3e843b5afce1b6a8fd3e8b0a603
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2021-11-08 19:50:19 +0500

    footer styles added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from 'react-router-dom';
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -8,59 +8,72 @@ const Footer = () => {
       <div className="container">
         <div className="site_footer__inner_wrapper">
           <div className="about_us">
-            <div className="">
-              <a className="about_us_link" href="">
-                About Us
-              </a>
-            </div>
-            {/* TODO: Link qanday yoziladi? */}
-            <div className="">
-              <a className="about_us_link" href="">
-                Advertising
-              </a>
-            </div>
+            <h4 className="site_footer__title">About Agoda</h4>
 
-            <p  className="all_rights">© 2021 Agoda LLC All rights reserved.</p>
+            <a className="about_us_link" href="">
+              {" "}
+              About Us{" "}
+            </a>
+            <a className="about_us_link" href="#">
+              {" "}
+              Advertising
+            </a>
+            <p className="all_rights">© 2021 Agoda LLC All rights reserved.</p>
           </div>
 
           <ul className="choices footer_list">
+          <h4 className="site_footer__title">Destinations</h4>
             <li className=" footer_list_item">
-              <a className="footer_list_link"  href="">Countires</a>
+              <a className="footer_list_link" href="">
+                Countrires
+              </a>
             </li>
             <li className=" footer_list_item">
-              <a className="footer_list_link" href="">Cities</a>
+              <a className="footer_list_link" href="">
+                Cities
+              </a>
             </li>
             <li className=" footer_list_item">
-              <a className="footer_list_link" href="">Hotels</a>
+              <a className="footer_list_link" href="">
+                Hotels
+              </a>
             </li>
           </ul>
 
           <ul className="more footer_list">
+          <h4 className="site_footer__title">Explore more</h4>
             <li className=" footer_list_item">
-              <a className="footer_list_link" href="">Reviews</a>
+              <a className="footer_list_link" href="">
+                {/* TODO:hreflarni tuldirish */}
+                Reviews
+              </a>
             </li>
             <li className=" footer_list_item">
-              <a className="footer_list_link" href="">Articles</a>
+              <a className="footer_list_link" href="">
+                Articles
+              </a>
             </li>
             <li className=" footer_list_item">
-              <a className="footer_list_link" href="">Ideas for Travel</a>
+              <a className="footer_list_link" href="">
+                Ideas for Travel
+              </a>
             </li>
           </ul>
 
           <ul className="socials footer_list">
             <li className=" footer_list_item">
               <a className="footer_list_link" href="">
-              <i class="fab fa-facebook"></i>
+                <i class="fab fa-facebook"></i>
               </a>
             </li>
             <li className=" footer_list_item">
               <a className="footer_list_link" href="">
-              <i class="fab fa-instagram"></i>
+                <i class="fab fa-instagram"></i>
               </a>
             </li>
             <li className=" footer_list_item">
               <a className="footer_list_link" href="">
-              <i class="fab fa-twitter"></i>
+                <i class="fab fa-twitter"></i>
               </a>
             </li>
           </ul>
