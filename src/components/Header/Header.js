8a9798fcefee8337a7483eac4636ed76129b3dcf
AbdugafurovaUmida <86460130+AbdugafurovaUmida@users.noreply.github.com>
2021-11-10 00:16:09 +0500

    valyuta buttonini dropdownga ozgartirdim

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,26 @@ import { Grid } from "@material-ui/core";
 import "./Header.css";
 
 const Header = () => {
+
+
+  const { useState, useEffect } = React;
+
+  const data = [{id: 0, label: "UZS"}, {id: 1, label: "DOLLAR"}];
+  
+  
+    const [isOpen, setOpen] = useState(false);
+    const [items, setItem] = useState(data);
+    const [selectedItem, setSelectedItem] = useState(null);
+    
+    const toggleDropdown = () => setOpen(!isOpen);
+    
+    const handleItemClick = (id) => {
+      selectedItem == id ? setSelectedItem(null) : setSelectedItem(id);
+    }
+  
+
+
+
   return (
     <header className="site__header">
       <div className="container  site__header__container">
@@ -17,7 +37,20 @@ const Header = () => {
             </Link>
           </Grid>
           <Grid item xs={2}>
-            <button className="currency" type='button'>UZS</button>
+          <div className='dropdown'>
+        <div className='dropdown-header currency' onClick={toggleDropdown}>
+          {selectedItem ? items.find(item => item.id == selectedItem).label : "UZS"}
+          <i className={`fa fa-chevron-right icon ${isOpen && "open"}`}></i>
+        </div>
+        <div className={`dropdown-body ${isOpen && 'open'}`}>
+          {items.map(item => (
+            <div className="dropdown-item" onClick={e => handleItemClick(e.target.id)} id={item.id}>
+              <span className={`dropdown-item-dot ${item.id == selectedItem && 'selected'}`}>• </span>
+              {item.label}
+            </div>
+          ))}
+        </div>
+      </div> {/* <button className="currency" type='button'>UZS</button> */}
             <button className="language" type='button'>
               <img className="language__image" src='img/russian-flag.png' />
               {/* <img src='img/anglish-flag.png'/> */}
