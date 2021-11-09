ba0864b1b1818a0227df71b5439b1280b1bed6ea
AbdugafurovaUmida <86460130+AbdugafurovaUmida@users.noreply.github.com>
2021-11-09 23:21:50 +0500

    headerdagi hoverlarni togirlab register-pageni tugatdim

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Grid } from "@material-ui/core";
+
+
 
 import "./Header.css";
 
@@ -7,30 +10,26 @@ const Header = () => {
   return (
     <header className="site__header">
       <div className="container  site__header__container">
-  
-          <Link className="site-header-logo" to='/' >
-            <img className="site-header-logo-img" src='img/agoda_logo.png' alt='site-header-logo' />
-          </Link>
-      
-        <div className="site__header__currency_and_language">
-          <button className="currency" type='button'>UZS</button>
-
-          <button className="language" type='button'>
-            <img className="language__image" src='img/russian-flag.png' />
-            {/* <img src='img/anglish-flag.png'/> */}
-          </button>
-        </div>
-
-        <div className="site-header__subgroup">
-          <div className="blog_pages">
+        <Grid direction="row" justifyContent="space-between" alignItems="center" container spacing={4}>
+          <Grid item xs={4}>
+            <Link className="site-header-logo" to='/' >
+              <img className="site-header-logo-img" src='img/agoda_logo.png' alt='site-header-logo' />
+            </Link>
+          </Grid>
+          <Grid item xs={2}>
+            <button className="currency" type='button'>UZS</button>
+            <button className="language" type='button'>
+              <img className="language__image" src='img/russian-flag.png' />
+              {/* <img src='img/anglish-flag.png'/> */}
+            </button>
+          </Grid>
+          <Grid container direction="row" justifyContent="flex-end" alignItems="center" item xs={6}>
             <Link className="blog_pages__review site-header-link" to='/review'><i class="fas fa-pen"></i>Review</Link>
-            <Link className="blog_pages__my_trips site-header-link"to='/my-trips'><i class="far fa-heart"></i>Trips</Link>
-          </div>
-          <div className="auth">
+            <Link className="blog_pages__my_trips site-header-link" to='/my-trips'><i class="far fa-heart"></i>Trips</Link>
             <Link className="auth__registration site-header-link" to="/register">Register</Link>
-            <Link className="auth__sign-in site-header-link"  to="/sign-in">Sign In</Link>
-          </div>
-        </div>
+            <Link className="auth__sign-in site-header-link" to="/sign-in">Sign In</Link>
+          </Grid>
+        </Grid>
       </div>
     </header>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,33 @@
 import React from "react";
+import './Register.css';
 
 const Register = () => {
   return (
-    <div className="register__page__wrapper">
-      <div className="container">Registration</div>
+    <div className="sign-up-page">
+      <div className="sign-up-form">
+        <h2 className="sign-up-form__title"> Register Account </h2>
+    <div className="register-form-content">
+      <form>
+        <div className="form-group">
+          <label className="form-group__label" for="username">Username</label>
+          <input className="form-group__input" type="text" id="username" name="username" required="required" />
+        </div>
+        <div className="form-group">
+          <label className="form-group__label" for="password">Password</label>
+          <input className="form-group__input" type="password" id="password" name="password" required="required"/>
+        </div>
+        <div className="form-group">
+          <label className="form-group__label" for="cpassword">Confirm Password</label>
+          <input className="form-group__input" type="password" id="cpassword" name="cpassword" required="required" />
+        </div>
+        <div className="form-group">
+          <label className="form-group__label" for="email">Email Address</label>
+          <input className="form-group__input" type="email" id="email" name="email" required="required" />
+        </div>
+          <button className="register-form__btn" type="submit">Register</button>
+      </form>
+    </div>
+      </div>
     </div>
   );
 };
